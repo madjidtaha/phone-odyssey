41f96303f7ff1f06bdc3e24385ad1708caefda78
Valentin Daguenet <valentin.daguenet@gmail.com>
2016-01-11 12:02:06 +0100

    go to browserify

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,21 @@
-import './stylus/main.styl';
 import socketConfig from 'config/socket.js';
 import domready from 'domready';
 import Webgl from './webgl/Webgl';
 import WebsocketClient from './WebsocketClient';
 import raf from 'raf';
+import dat from 'dat-gui';
 
 const socketServer = new WebsocketClient(socketConfig.url);
 const webgl = new Webgl(window.innerWidth, window.innerHeight);
+const gui = new dat.GUI();
 
 domready(() => {
   document.body.appendChild(webgl.renderer.domElement);
+
+
+  gui.add(webgl.params, 'usePostprocessing');
+  gui.add(webgl.params, 'vignette');
+
   // handle resize
   window.onresize = resizeHandler;
   // let's play !
--- client/src/webgl/controls/VintagePhoneControls.js
+++ client/src/webgl/controls/VintagePhoneControls.js
@@ -1,10 +1,13 @@
 export default class VintagePhoneControls {
-  constructor(camera) {
-    this.camera = camera;
+  constructor(object) {
+    this.object = object;
 
     this.toX = 0;
     this.toY = 0;
     this.toZ = 0;
+
+    this.intencityReduction = 0.1;
+    this.dampingFactor = 0.1;
 
     this.originalPos = undefined;
   }
@@ -16,16 +19,16 @@ export default class VintagePhoneControls {
       return;
     }
 
-    this.toX = pos.x - this.originalPos.x;
-    this.toY = pos.y - this.originalPos.y;
-    this.toZ = pos.z - this.originalPos.z;
+    this.toX = (pos.x - this.originalPos.x) * this.intencityReduction;
+    this.toY = (pos.y - this.originalPos.y) * this.intencityReduction;
+    this.toZ = (pos.z - this.originalPos.z) * this.intencityReduction;
   }
 
   updateFromGyro(ax, ay, az) {}
 
   update() {
-    this.camera.position.x += (this.toX - this.camera.position.x) * 0.1;
-    this.camera.position.y += (this.toY - this.camera.position.y) * 0.1;
-    // this.camera.position.z += (this.toZ - this.camera.position.z) * 0.1;
+    this.object.position.x += (this.toX - this.object.position.x) * this.dampingFactor;
+    this.object.position.y += (this.toY - this.object.position.y) * this.dampingFactor;
+    // this.object.position.z += (this.toZ - this.object.position.z) * this.dampingFactor;
   }
 }
